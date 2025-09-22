6d9ee69a80648c81c909d34365d1ed597dea6707
Shehara Navarathna <it21017506@my.sliit.lk>
2025-09-22 10:22:22 +0530

    Added basic homepage and security setup

---
+++ src/main/java/com/sims/HomeController.java
@@ -0,0 +1,13 @@
+package com.sims;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // This will look for index.html in src/main/resources/templates
+    }
+}
